a9160cbe0ac0d2bafcd6381ee8d1651c5e2e0fc9
TobiasAAU <2biasbl@gmail.com>
2023-11-29 01:00:15 +0100

    fixed chat and prepped for backend

--- src/Components/ChatOutputComponent.tsx
+++ src/Components/ChatOutputComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 type ChatOutputComponentProps = {
   chatMessages: ChatMessage[];
@@ -10,19 +10,40 @@ type ChatMessage = {
   sender: 'user' | 'backend';
 };
 
-const ChatOutputComponent: React.FC<ChatOutputComponentProps> = ({ chatMessages, isMessagesVisible }) => {
-    const maxHeightWhenHidden = '10vh'; // Adjust this value as needed
-    const maxHeight = isMessagesVisible ? '65vh' : maxHeightWhenHidden;
-  
-    return (
-      <div className={`rounded-lg overflow-y-auto pl-4 w-7/8 bg-olive`} style={{ maxHeight }}>
-        {chatMessages.map((message, index) => (
-          <p key={index} className="text-lg text-gray-900">
-            {message.text}
-          </p>
-        ))}
-      </div>
-    );
+const ChatOutputComponent: React.FC<ChatOutputComponentProps> = ({
+  chatMessages,
+  isMessagesVisible,
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const maxHeightWhenHidden = '10vh'; // Adjust this value as needed
+  const maxHeight = isMessagesVisible ? '65vh' : maxHeightWhenHidden;
+
+  // Function to scroll to the bottom of the chat log
+  const scrollToBottom = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
   };
+
+  // Scroll to the bottom whenever the chat messages change
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatMessages]);
+
+  return (
+    <div
+      ref={containerRef}
+      className={`rounded-lg overflow-y-auto pl-4 w-7/8 bg-olive`}
+      style={{ maxHeight }}
+    >
+      {chatMessages.map((message, index) => (
+        <p key={index} className="text-lg text-gray-900">
+          {message.text}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 export default ChatOutputComponent;
