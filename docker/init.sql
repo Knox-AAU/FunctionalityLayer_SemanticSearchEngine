4ef27c727b5199d54dcc46113ead7c9c6083490b
WiredG1 <thomas.nykjaer@live.dk>
2023-12-07 12:24:06 +0100

    Docker fix

--- docker/init.sql
+++ docker/init.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS pdfTable (
     title VARCHAR(1023),
     url VARCHAR(1023) UNIQUE,
     pdfPath VARCHAR(1023),
+    stringPath VARCHAR(1023),
     author VARCHAR(255),
     timestamp bigint
 );
@@ -21,3 +22,4 @@ CREATE USER defaultuser WITH PASSWORD '1234';
 
 -- Grant insert and select permissions on the table to the user
 GRANT INSERT, SELECT ON pdfTable TO defaultuser;
+GRANT INSERT, SELECT ON spoTable TO defaultuser;
