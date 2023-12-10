681c1ae04fa647096d65dbb2d456c43cb1944e3e
WiredG1 <thomas.nykjaer@live.dk>
2023-12-11 00:24:35 +0100

    Added noLlama

--- src/screens/Mainscreen.tsx
+++ src/screens/Mainscreen.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import SearchBarComponent from '../Components/SearchBarComponent';
 import {
-	sortPdfObjectsByDate,
-	sortPdfObjectsByRelevance,
+  sortPdfObjectsByDate,
+  sortPdfObjectsByRelevance,
 } from '../TypesAndLogic/SortingLogic2';
 import ChatComponent from '../Components/ChatComponent';
 import SearchResultComponent from '../Components/SearchResultComponent';
@@ -12,161 +12,210 @@ import { pdfObjectArray } from '../TypesAndLogic/Types';
 import { pdfObject } from '../TypesAndLogic/Types';
 
 const Mainscreen = () => {
-	let navigate = useNavigate();
-	const routeChange = (path: string) => {
-		navigate(path);
-	};
-	const [PdfObjects, setPdfObjects] = useState<pdfObject[]>([]);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState<string | null>(null);
-	const [sortOrder, setSortOrder] = useState<'ascending' | 'descending'>(
-		'ascending'
-	);
+  let navigate = useNavigate();
+  const routeChange = (path: string) => {
+    navigate(path);
+  };
+  const [PdfObjects, setPdfObjects] = useState<pdfObject[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [sortOrder, setSortOrder] = useState<'ascending' | 'descending'>(
+    'ascending'
+  );
 
-	const handleSortByDate = useCallback(() => {
-		const sortedPdfObjects = sortPdfObjectsByDate(PdfObjects, sortOrder);
-		setPdfObjects(sortedPdfObjects);
-		setSortOrder((prevSortOrder: string) =>
-			prevSortOrder === 'ascending' ? 'descending' : 'ascending'
-		);
-	}, [PdfObjects, sortOrder]);
+  const handleSortByDate = useCallback(() => {
+    const sortedPdfObjects = sortPdfObjectsByDate(PdfObjects, sortOrder);
+    setPdfObjects(sortedPdfObjects);
+    setSortOrder((prevSortOrder: string) =>
+      prevSortOrder === 'ascending' ? 'descending' : 'ascending'
+    );
+  }, [PdfObjects, sortOrder]);
 
-	const handleSortByRelevance = useCallback(() => {
-		const sortedPdfObjects = sortPdfObjectsByRelevance(PdfObjects);
-		setPdfObjects(sortedPdfObjects);
-	}, [PdfObjects]);
+  const handleSortByRelevance = useCallback(() => {
+    const sortedPdfObjects = sortPdfObjectsByRelevance(PdfObjects);
+    setPdfObjects(sortedPdfObjects);
+  }, [PdfObjects]);
 
-	const handleSearch = async (searchParams: {
-		query: string;
-		publishedAfter?: string;
-		publishedBefore?: string;
-		author?: string;
-		titleSearch?: boolean;
-	}) => {
-		setLoading(true);
-		setError(null);
+  const handleSearch = async (searchParams: {
+    query: string;
+    publishedAfter?: string;
+    publishedBefore?: string;
+    author?: string;
+    titleSearch?: boolean;
+  }) => {
+    setLoading(true);
+    setError(null);
 
-		const options = {
-			url: '/search',
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(searchParams),
-			timeout: 720000,
-		};
+    const options = {
+      url: '/search',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(searchParams),
+      timeout: 720000,
+    };
 
-		fetch(options.url, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				//'Keep-Alive': 'timeout=720, max=1',
-			},
-			body: JSON.stringify(searchParams),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(JSON.stringify({ query: searchParams }));
-				console.log('Changing Data');
-				console.log(JSON.stringify(data));
-				//"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
-				setPdfObjects(
-					data.map((element: any) => {
-						return {
-							url: element.URL,
-							date: element.TimeStamp,
-							relevance: element.Score,
-							title: element.Title,
-						};
-					})
-				);
-				setLoading(false);
-			})
-			.catch((err) => {
-				alert(err);
-				console.log(err);
-			});
-	};
+    fetch(options.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        //'Keep-Alive': 'timeout=720, max=1',
+      },
+      body: JSON.stringify(searchParams),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(JSON.stringify({ query: searchParams }));
+        console.log('Changing Data');
+        console.log(JSON.stringify(data));
+        //"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
+        setPdfObjects(
+          data.map((element: any) => {
+            return {
+              url: element.URL,
+              date: element.TimeStamp,
+              relevance: element.Score,
+              title: element.Title,
+            };
+          })
+        );
+        setLoading(false);
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
+  };
+  const handleBertSearch = async (searchParams: {
+    query: string;
+    publishedAfter?: string;
+    publishedBefore?: string;
+    author?: string;
+    titleSearch?: boolean;
+  }) => {
+    setLoading(true);
+    setError(null);
 
-	const handleChat = async (query: string) => {
-		// Log the chat request details
-		console.log(`Chat Request: ${query}`);
+    const options = {
+      url: '/bertSearch',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(searchParams),
+      timeout: 720000,
+    };
 
-		// Post the user's query in the chat
-		setChatMessages((prevMessages) => [
-			...prevMessages,
-			{ text: query, sender: 'user' },
-		]);
+    fetch(options.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        //'Keep-Alive': 'timeout=720, max=1',
+      },
+      body: JSON.stringify(searchParams),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(JSON.stringify({ query: searchParams }));
+        console.log('Changing Data');
+        console.log(JSON.stringify(data));
+        //"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
+        setPdfObjects(
+          data.map((element: any) => {
+            return {
+              url: element.URL,
+              date: element.TimeStamp,
+              relevance: element.Score,
+              title: element.Title,
+            };
+          })
+        );
+        setLoading(false);
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
+  };
 
-		try {
-			// Send the user's query to the chatbot backend
-			const response = await fetch('/search', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ query }),
-			});
+  const handleChat = async (query: string) => {
+    // Log the chat request details
+    console.log(`Chat Request: ${query}`);
 
-			if (!response.ok) {
-				throw new Error('Failed to get a response from the chatbot backend');
-			}
+    // Post the user's query in the chat
+    setChatMessages((prevMessages) => [
+      ...prevMessages,
+      { text: query, sender: 'user' },
+    ]);
 
-			// Example: Assuming the backend responds with a JSON object containing the bot's reply
-			const responseData = await response.json();
+    try {
+      // Send the user's query to the chatbot backend
+      const response = await fetch('/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      });
 
-			// Post the response from the backend in the chat
-			setChatMessages((prevMessages) => [
-				...prevMessages,
-				{ text: responseData.response, sender: 'backend' },
-			]);
-		} catch (error) {
-			console.error('Error during chat:', error);
-			// Handle errors, e.g., display an error message in the chat
+      if (!response.ok) {
+        throw new Error('Failed to get a response from the chatbot backend');
+      }
 
-			// If there is an error or no response from the backend, post a dummy response
-			setChatMessages((prevMessages) => [
-				...prevMessages,
-				{
-					text: "Sorry, I couldn't understand that, or maybe there is no connection to the backend. Please try again.",
-					sender: 'backend',
-				},
-			]);
-		}
-	};
+      // Example: Assuming the backend responds with a JSON object containing the bot's reply
+      const responseData = await response.json();
 
-	const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+      // Post the response from the backend in the chat
+      setChatMessages((prevMessages) => [
+        ...prevMessages,
+        { text: responseData.response, sender: 'backend' },
+      ]);
+    } catch (error) {
+      console.error('Error during chat:', error);
+      // Handle errors, e.g., display an error message in the chat
 
-	useEffect(() => {
-		// Initial load
-	}, []);
+      // If there is an error or no response from the backend, post a dummy response
+      setChatMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          text: "Sorry, I couldn't understand that, or maybe there is no connection to the backend. Please try again.",
+          sender: 'backend',
+        },
+      ]);
+    }
+  };
 
-	return (
-		<div className="main-container h-screen mx-auto overflow-y-auto bg-gray-900">
-			<button onClick={() => routeChange('/admin')}>Admin</button>
-			<div className="title-line text-xl font-bold p-4 text-white pl-4">
-				KNOX Search and Chat
-			</div>
-			<div className="Chatcomponent">
-				<ChatComponent chatMessages={chatMessages} onChat={handleChat} />
-			</div>
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
 
-			<div className="Searchcomponent">
-				<SearchBarComponent
-					onSearch={(searchParams) => handleSearch(searchParams)}
+  useEffect(() => {
+    // Initial load
+  }, []);
+
+  return (
+    <div className="main-container h-screen mx-auto overflow-y-auto bg-gray-900">
+      <button onClick={() => routeChange('/admin')}>Admin</button>
+      <div className="title-line text-xl font-bold p-4 text-white pl-4">
+        KNOX Search and Chat
+      </div>
+      <div className="Chatcomponent">
+        <ChatComponent chatMessages={chatMessages} onChat={handleChat} />
+      </div>
+
+      <div className="Searchcomponent">
+        <SearchBarComponent
+          onSearch={(searchParams) => handleSearch(searchParams)}
           onBertSearch={(searchParams) => handleBertSearch(searchParams)}
-					onSortByDate={handleSortByDate}
-					onSortByRelevance={handleSortByRelevance}
-					sortOrder={sortOrder}
-				/>
-				{loading && <div>Loading...</div>}
-				{!loading && <SearchResultComponent PdfObjects={PdfObjects} />}
-			</div>
-		</div>
-	);
+          onSortByDate={handleSortByDate}
+          onSortByRelevance={handleSortByRelevance}
+          sortOrder={sortOrder}
+        />
+        {loading && <div>Loading...</div>}
+        {!loading && <SearchResultComponent PdfObjects={PdfObjects} />}
+      </div>
+    </div>
+  );
 };
 
 export default Mainscreen;
 
 type ChatMessage = {
-	text: string;
-	sender: 'user' | 'backend';
+  text: string;
+  sender: 'user' | 'backend';
 };
