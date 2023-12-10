a68cfb3c6a204baf08ce103b5c43d4f6e44369a7
WiredG1 <thomas.nykjaer@live.dk>
2023-12-10 19:53:03 +0100

    AAAA

--- src/Components/SearchBarComponent.tsx
+++ src/Components/SearchBarComponent.tsx
@@ -3,18 +3,19 @@ import React, { useState } from 'react';
 
 type SearchBarComponentProps = {
     onSearch: (searchParams: { query: string, publishedAfter: string, publishedBefore: string }) => void;
+    onBertSearch: (searchParams: { query: string, publishedAfter: string, publishedBefore: string }) => void;
     onSortByDate: () => void;
     onSortByRelevance: () => void;
     sortOrder: 'ascending' | 'descending';
-  };
+};
 
-const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSortByDate, onSortByRelevance, sortOrder }) => {
+const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onBertSearch, onSortByDate, onSortByRelevance, sortOrder }) => {
     const [query, setQuery] = useState('');
     const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
     const [searchOption, setSearchOption] = useState('Standardsearch');
     const [language, setLanguage] = useState('autodetect');
 
-   
+
 
     const handleSortByDate = () => {
         onSortByDate(); // Call the passed function without arguments
@@ -29,7 +30,7 @@ const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSor
     };
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-            handleSearch();
+            handleBertSearch();
         }
     };
     const [publishedAfter, setPublishedAfter] = useState('');
@@ -37,26 +38,34 @@ const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSor
 
     const handleSearch = () => {
         if (query) {
-          onSearch({ query, publishedAfter, publishedBefore });
+            onSearch({ query, publishedAfter, publishedBefore });
         }
-      };
+    };
+    const handleBertSearch = () => {
+        if (query) {
+            onBertSearch({ query, publishedAfter, publishedBefore });
+        }
+    };
     return (
         <div className="search-bar ml-8 border-2 border-gray-900 rounded-lg mb-2 w-4/5 pl-6 py-2  bg-darkgrey">
-            <input 
-                type="text" 
-                value={query} 
-                onChange={(e) => setQuery(e.target.value)} 
+            <input
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
                 onKeyDown={handleKeyDown}
                 className="border p-2 mr-2 w-4/5"
             />
             <button onClick={handleSearch} className="bg-blue-500 text-white p-2 rounded mr-2">
                 Search
-            </button>  
-                       <button onClick={onSortByDate} className="bg-rebeccapurple text-white pl-2 pr-2 rounded mr-2 mt-0.5 h-8">
+            </button>
+            <button onClick={handleBertSearch} className="bg-blue-500 text-white p-2 rounded mr-2">
+                No LlamaSearch
+            </button>
+            <button onClick={onSortByDate} className="bg-rebeccapurple text-white pl-2 pr-2 rounded mr-2 mt-0.5 h-8">
                 Sort by Date {sortOrder === 'ascending' ? '↑' : '↓'}
             </button>
             <button onClick={handleSortByRelevance} className="bg-rebeccapurple text-white pl-2 pr-2 rounded mr-2 mt-0.5 h-8">Sort by Relevance</button>
-            
+
             <button onClick={toggleAdvancedOptions} className="bg-gray-500 text-white p-2 rounded ml-2">
                 Advanced
             </button>
@@ -64,33 +73,33 @@ const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSor
             {showAdvancedOptions && (
                 <div className="advanced-options mt-2 flex flex-wrap">
                     {/* Radio Buttons */}
-                    
+
                     <div className="flex items-center mr-4">
                         <label>Author:</label>
-                        <input 
-                            value="Bruce Wayne" 
-                            type="text" 
-                            checked={searchOption === 'Authorsearch'} 
-                            onChange={(e) => setSearchOption(e.target.value)} 
+                        <input
+                            value="Bruce Wayne"
+                            type="text"
+                            checked={searchOption === 'Authorsearch'}
+                            onChange={(e) => setSearchOption(e.target.value)}
                             className="mr-1"
                         />
                     </div>
                     <div className="flex items-center mr-4">
-                        <input 
-                            type="radio" 
-                            value="Titlesearch" 
-                            checked={searchOption === 'Titlesearch'} 
-                            onChange={(e) => setSearchOption(e.target.value)} 
+                        <input
+                            type="radio"
+                            value="Titlesearch"
+                            checked={searchOption === 'Titlesearch'}
+                            onChange={(e) => setSearchOption(e.target.value)}
                             className="mr-1"
                         />
                         <label>Search titles only</label>
                     </div>
                     <div className="flex items-center mr-4">
-                        <input 
-                            type="radio" 
-                            value="Standardsearch" 
-                            checked={searchOption === 'Standardsearch'} 
-                            onChange={(e) => setSearchOption(e.target.value)} 
+                        <input
+                            type="radio"
+                            value="Standardsearch"
+                            checked={searchOption === 'Standardsearch'}
+                            onChange={(e) => setSearchOption(e.target.value)}
                             className="mr-1"
                         />
                         <label>Normal KNOX search</label>
@@ -99,9 +108,9 @@ const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSor
                     {/* Dropdown Menu for Language Selection */}
                     <div className="flex items-center mr-4">
                         <label className="mr-2">Language:</label>
-                        <select 
-                            value={language} 
-                            onChange={(e) => setLanguage(e.target.value)} 
+                        <select
+                            value={language}
+                            onChange={(e) => setLanguage(e.target.value)}
                             className="border p-1 rounded"
                         >
                             <option value="danish">Danish</option>
@@ -109,12 +118,12 @@ const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSor
                             <option value="autodetect">auto detect</option>
                         </select>
                     </div>
-                   
+
                     <input
-                    type="date"
-                    value={publishedAfter}
-                    onChange={(e) => setPublishedAfter(e.target.value)}
-                    className="border p-2 mr-2"
+                        type="date"
+                        value={publishedAfter}
+                        onChange={(e) => setPublishedAfter(e.target.value)}
+                        className="border p-2 mr-2"
                     />
                     <input
                         type="date"
@@ -122,7 +131,7 @@ const SearchBarComponent: React.FC<SearchBarComponentProps> = ({ onSearch, onSor
                         onChange={(e) => setPublishedBefore(e.target.value)}
                         className="border p-2 mr-2"
                     />
-                   
+
                 </div>
             )}
         </div>
--- src/screens/Mainscreen.tsx
+++ src/screens/Mainscreen.tsx
@@ -54,25 +54,37 @@ const Mainscreen = () => {
     console.log(`Search Request Body: ${JSON.stringify({ searchParams })}`);
 
     try {
-      const response = await fetch(`/search`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ "query": searchParams }),
-      });
-      if (!response.ok) {
-        alert("ERR" + response.status);
-      } else {
-        response.json()
-          .then((data) => {
-            console.log("Changing Data");
-            console.log(JSON.stringify(data));
-            //"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
-            setPdfObjects(data.map((element: any) => { return { "url": element.URL, "date": element.TimeStamp, "relevance": element.Score, "title": element.Title } }));
-            setLoading(false);
-          });
+      const data = await TimeoutWrapper(options);
+      console.log(JSON.stringify({ "query": searchParams }));
+      console.log("Changing Data");
+      console.log(JSON.stringify(data));
+      //"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
+      setPdfObjects(data.map((element: any) => { return { "url": element.URL, "date": element.TimeStamp, "relevance": element.Score, "title": element.Title } }));
+      setLoading(false);
 
+    } catch (err) {
+      alert(err);
+      console.log(err);
+    }
+  }
+  const handleBertSearch = async (searchParams: { query: string; publishedAfter?: string; publishedBefore?: string; author?: string; titleSearch?: boolean; }) => {
+    setLoading(true);
+    setError(null);
 
-      }
+    const options = {
+      url: '/bertSearch',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(searchParams),
+      timeout: 720000,
+    };
+
+    try {
+      const data = await TimeoutWrapper(options);
+      console.log("Changing Data");
+      console.log(JSON.stringify(data));
+      setPdfObjects(data.map((element: any) => { return { "url": element.URL, "date": element.TimeStamp, "relevance": element.Score, "title": element.Title } }));
+      setLoading(false);
     } catch (err) {
       alert(err);
       console.log(err);
@@ -136,7 +148,9 @@ const Mainscreen = () => {
 
       <div className='Searchcomponent'>
         <SearchBarComponent
-          onSearch={(searchParams) => fetchSearchResults(searchParams)}
+
+          onSearch={(searchParams) => handleSearch(searchParams)}
+          onBertSearch={(searchParams) => handleBertSearch(searchParams)}
           onSortByDate={handleSortByDate}
           onSortByRelevance={handleSortByRelevance}
           sortOrder={sortOrder}
