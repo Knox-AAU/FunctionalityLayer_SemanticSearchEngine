0ea54e91b328d9f77051406e7552d4b8187b1ac2
WiredG1 <thomas.nykjaer@live.dk>
2023-12-10 19:53:57 +0100

    Fixed

--- src/screens/Mainscreen.tsx
+++ src/screens/Mainscreen.tsx
@@ -50,20 +50,37 @@ const Mainscreen = () => {
     };
 
     try {
-      const response = await TimeoutWrapper(options);
+      const data = await TimeoutWrapper(options);
       console.log(JSON.stringify({ "query": searchParams }));
-      if (!response.ok) {
-        alert("ERR" + response.status);
-      } else {
-        response.json()
-          .then((data: any) => {
-            console.log("Changing Data");
-            console.log(JSON.stringify(data));
-            //"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
-            setPdfObjects(data.map((element: any) => { return { "url": element.URL, "date": element.TimeStamp, "relevance": element.Score, "title": element.Title } }));
-            setLoading(false);
-          });
-      }
+      console.log("Changing Data");
+      console.log(JSON.stringify(data));
+      //"URL": doc.get("url"), "pdfPath": doc.get("pdfPath"), "Title": doc.get("title"), "Score": score
+      setPdfObjects(data.map((element: any) => { return { "url": element.URL, "date": element.TimeStamp, "relevance": element.Score, "title": element.Title } }));
+      setLoading(false);
+
+    } catch (err) {
+      alert(err);
+      console.log(err);
+    }
+  }
+  const handleBertSearch = async (searchParams: { query: string; publishedAfter?: string; publishedBefore?: string; author?: string; titleSearch?: boolean; }) => {
+    setLoading(true);
+    setError(null);
+
+    const options = {
+      url: '/bertSearch',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(searchParams),
+      timeout: 720000,
+    };
+
+    try {
+      const data = await TimeoutWrapper(options);
+      console.log("Changing Data");
+      console.log(JSON.stringify(data));
+      setPdfObjects(data.map((element: any) => { return { "url": element.URL, "date": element.TimeStamp, "relevance": element.Score, "title": element.Title } }));
+      setLoading(false);
     } catch (err) {
       alert(err);
       console.log(err);
@@ -129,7 +146,7 @@ const Mainscreen = () => {
         <SearchBarComponent
 
           onSearch={(searchParams) => handleSearch(searchParams)}
-
+          onBertSearch={(searchParams) => handleBertSearch(searchParams)}
           onSortByDate={handleSortByDate}
           onSortByRelevance={handleSortByRelevance}
           sortOrder={sortOrder}
