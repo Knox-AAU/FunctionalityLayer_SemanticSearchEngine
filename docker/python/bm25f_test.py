d76103eeaaad6ca15746307481a824154dde7ded
TobiasAAU <2biasbl@gmail.com>
2023-12-18 23:17:25 +0100

    nyt doc-array i bm25f_test

--- docker/python/bm25f_test.py
+++ docker/python/bm25f_test.py
@@ -13,17 +13,21 @@ print("HELLO Testers")
 
 class unitTesterClass(unittest.TestCase):
     docArray = [
-        {
-            "title": "Barack Obama",
-            "body": "Michelle Obama is husband and has been living in the whitehouse in washinton for a long time"
+         {
+            "title": ["Barack Obama"],
+            "body": ["Michelle Obama is husband and has been living in the whitehouse in washinton for a long time"]
         },
+        
+        # 24 x gotham , 0 x total
         {
-            "title": "Bruce Wayne",
-            "body": "Gotham City, shrouded in darkness, faced a new threat. The notorious villain, Riddler, unleashed a series of mind-bending puzzles across the city. As chaos ensued, Commissioner Gordon turned to the shadows for help. Silently, Batman emerged from the shadows, his cape billowing in the night wind. With his brilliant mind and unparalleled detective skills, he deciphered the intricate riddles, leading him to the heart of Riddler\'s lair. In a pulse-pounding confrontation, Batman outsmarted the Riddler's traps and confronted the villain. As the Bat-Signal pierced the sky, signaling justice restored, Gotham City sighed in relief, knowing that as long as there were shadows, the Dark Knight is be there to protect them"       
+            "title": ["villains"],
+            "body":["Gotham City Gotham Gotham Gotham Gotham Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City has 2003 villains, and it is remarkable that every single civilian living in gotham has not just moved out already."]        
         },
+        
+        # 20 x gotham ,  2 x total
         {
-            "title": "villains",
-            "body": "Gotham City has a total of 2003 villains, and it is remarkable that every single civilian living in gotham has not just moved out already."       
+            "title": ["villains"],
+            "body":["Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City Gotham City has a total of 2003 villains, and it is total remarkable that every single civilian living in gotham has not just moved out already."]        
         }
     ]
     BM25FInstance = BM25F(docArray, {"title": 0.5, "body": 0.5})
