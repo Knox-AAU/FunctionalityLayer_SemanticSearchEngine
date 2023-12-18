8f6e2b83cdbd35d5e16c09842f97c5222361f113
TobiasAAU <2biasbl@gmail.com>
2023-12-18 16:37:00 +0100

    missed a ,

--- docker/python/bm25f_test.py
+++ docker/python/bm25f_test.py
@@ -20,7 +20,7 @@ class unitTesterClass(unittest.TestCase):
         {
             "title": ["Bruce Wayne"],
             "body":["Gotham City, shrouded in darkness, faced a new threat. The notorious villain, Riddler, unleashed a series of mind-bending puzzles across the city. As chaos ensued, Commissioner Gordon turned to the shadows for help. Silently, Batman emerged from the shadows, his cape billowing in the night wind. With his brilliant mind and unparalleled detective skills, he deciphered the intricate riddles, leading him to the heart of Riddler\'s lair. In a pulse-pounding confrontation, Batman outsmarted the Riddler's traps and confronted the villain. As the Bat-Signal pierced the sky, signaling justice restored, Gotham City sighed in relief, knowing that as long as there were shadows, the Dark Knight is be there to protect them"]        
-        }
+        },
         {
             "title": ["villains"],
             "body":["Gotham City has a total of 2003 villains, and it is remarkable that every single civilian living in gotham has not just moved out already."]        
